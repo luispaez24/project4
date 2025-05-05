dc705287d365e9409b6f06c48a36f08f47ff6597
Donsch <67032252+Donsch377@users.noreply.github.com>
2025-05-05 15:55:08 -0600

    Added functions

--- Main.lisp
+++ Main.lisp
@@ -129,11 +129,60 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; ***** #7 Bi-Implication Function *****
+(defun boolean-iff (a b)
+  (or (and a b)
+      (and (not a) (not b)))
+)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; ***** #8 Boolean-Expression Function *****
+(defun boolean-eval (exp)
+  (cond
+    ((or (equal exp t) (equal exp nil)) exp)
+    ((equal (car exp) 'not)
+     (not (boolean-eval (second exp))))
+    ((equal (car exp) 'and)
+     (let ((args (cdr exp)))
+       (cond ((null args) t)
+             ((equal (boolean-eval (car args)) nil) nil)
+             (t (boolean-eval (cons 'and (cdr args)))))))
+    ((equal (car exp) 'or)
+     (let ((args (cdr exp)))
+       (cond ((null args) nil)
+             ((equal (boolean-eval (car args)) t) t)
+             (t (boolean-eval (cons 'or (cdr args)))))))
+    ((equal (car exp) 'xor)
+     (let ((a (boolean-eval (second exp)))
+           (b (boolean-eval (third exp))))
+       (or (and a (not b))
+           (and (not a) b))))
+    ((equal (car exp) 'implies)
+     (let ((a (boolean-eval (second exp)))
+           (b (boolean-eval (third exp))))
+       (or (not a) b)))
+    ((equal (car exp) 'iff)
+     (boolean-iff (boolean-eval (second exp))
+                  (boolean-eval (third exp))))
+    (t nil))
+)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; ***** #9 Merge-Sort Function *****
+(defun merge-sort (list predicate)
+  (labels ((merge-lists (left right)
+             (cond
+               ((null left) right)
+               ((null right) left)
+               ((funcall predicate (car left) (car right))
+                (cons (car left) (merge-lists (cdr left) right)))
+               (t
+                (cons (car right) (merge-lists left (cdr right)))))))
+    (if (or (null list) (null (cdr list)))
+        list
+        (let* ((mid (floor (length list) 2))
+               (left (subseq list 0 mid))
+               (right (subseq list mid)))
+          (merge-lists (merge-sort left predicate)
+                       (merge-sort right predicate))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
